152406ffe4bf083bef60ceb80f59c2de7dbe40e5
s.yamazaki <yamazaki@k-cb.co.jp>
2016-01-04 10:51:40 +0900

    Add ip-messenger cask

---
+++ ip-messenger.rb
@@ -0,0 +1,7 @@
+class IpMessenger < Cask
+  url 'http://ishwt.net/downloads/IPMessenger0B5.dmg'
+  homepage 'http://ishwt.net/software/ipmsg/'
+  version '0.11.5'
+  no_checksum
+  link 'IPMessenger.app'
+end
